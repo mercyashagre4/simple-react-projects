53a8eac205c12bfefaa843980253e7e9ad40e61f
mercy <118175416+mercyashagre4@users.noreply.github.com>
2024-02-10 02:48:31 -0800

    Random Joke Generator

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,8 @@
-// import { Routes, Route, Link, Outlet } from "react-router-dom";
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
-// import FirstReact from './components/FirstReact'
-// import ListUser from './components/ListUser';
-// import CreateUser from './components/CreateUser';
-// import ErrorPage from "./components/ErrorPage";
-// import { RouterProvider, createBrowserRouter } from "react-router-dom";
-
-// const router = createBrowserRouter([
-//   {
-//     path:"/",
-//     element: <ListUser/>
-//   },
-//   {
-//     path: "user/noo",
-//     element: <CreateUser/>
-//   },
-//   {
-//     path: "*", // the * repesents any other page not provided by the router
-//     element: <ErrorPage />
-//   }
-// ]);
-
-// function App() {
-  
-
-//   return (
-//     <div className="bg-red">
-//       <FirstReact/>
-//       <h5 className="text-7xl text-center text-blue-400">hello tailwind css</h5> 
-//       <RouterProvider router={router}/>
-      
-//       <Outlet/>
-      
-//     </div>
-//   );
-// }
-
-// export default App
 
 /* ===============Random Joke generator ===============*/
 
 import React from 'react';
-import JokeGenerator from './components/JokeGenerator';
+import JokeGenerator from './components/JokeGenerator'; // importing JokeGenerator to be used by App.jsx
 
 function App() {
   return (

1a7980a61b1895d506e1622a80d5828dbd183031
mercy <118175416+mercyashagre4@users.noreply.github.com>
2024-02-15 04:45:48 -0800

    Add files via upload

---
+++ Restaurant landing page/src/layouts/ReviewCard.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const ReviewCard = (props) => {
+  return (
+    <div className=" w-full md:w-1/3 bg-white border-2 border-lightText md:border-none p-5 rounded-lg shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px]">
+      <div>
+        <p className=" text-lightText">
+        Good Foodie Place with Unfortunate Disaster Lying in Wait Great food, though one potentially dangerous incident
+         occurred... We had a good time here experiencing Ethiopian food, culture, dance, and singing. 
+         People (both staff and customers) were friendly and happy to be in the packed restaurant
+        </p>
+       
+      </div>
+
+        <div className=" flex flex-row justify-center items-center mt-4 gap-4">
+        <img className=" rounded-full w-1/4" src={props.img} alt="img" />
+        <h3 className=" font-semibold ">{props.name}</h3>
+      </div>
+    </div>
+  );
+};
+
+export default ReviewCard;

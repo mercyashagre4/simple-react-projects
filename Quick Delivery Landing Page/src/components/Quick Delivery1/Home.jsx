679ab815fa2143f600f4e56a12a0b243ce3ed66c
mercy <118175416+mercyashagre4@users.noreply.github.com>
2024-03-28 15:17:19 -0700

    Update Home.jsx

--- Quick Delivery Landing Page/src/components/Quick Delivery1/Home.jsx
+++ Quick Delivery Landing Page/src/components/Quick Delivery1/Home.jsx
@@ -1,3 +1,4 @@
+/*========================================= Home ==================================*/
 import React from 'react';
 import quickBackground from '../../assets/quickImages/quickBackground.jpg';
 import './Quick Deliverycss/Home.css';
@@ -9,7 +10,7 @@ function Home() {
         <h1>Simplify your life with our efficient <span>delivery solutions</span> </h1>
         <button type='button' className='button1'>Order Now</button>
     </div>
-  )
+  );
 }
 
 export default Home;
